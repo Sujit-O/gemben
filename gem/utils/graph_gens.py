b4899206721a943cb07ef1d2b71b58af0865cb91
DI HUANG <deehuang0220@gmail.com>
2019-03-26 00:17:41 -0700

    correct typo

--- gem/utils/graph_gens.py
+++ gem/utils/graph_gens.py
@@ -104,7 +104,7 @@ def barabasi_albert_graph(N, deg, dia, dim):
 
     end_time = time()
 
-    print('Graph_Name: barabase_albert_graph')
+    print('Graph_Name: barabasi_albert_graph')
     print('Num_Nodes: ', nx.number_of_nodes(best_G), ' Avg_Deg : ', best_avg_deg, ' Diameter: ', best_diam)
     print('TIME: ' , end_time - strt_time, ' secs')
 
