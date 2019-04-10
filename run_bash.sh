b3b7ba8be6d818d9df76523a7b0bf8431e2ee4aa
Di Huang <dh_599@hpc-login3.hpcc.usc.edu>
2019-04-09 21:40:35 -0700

    source cmake

--- run_bash.sh
+++ run_bash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-for DOM in social internet biology; do
-    for METH in rand aa gf hope; do
+for DOM in internet ; do
+    for METH in lap; do
         sbatch --export=METH=$METH,DOM=$DOM exp_16core_benchmark.slurm
     done
-    sbatch --export=METH=sdne,DOM=$DOM exp_1gpu_benchmark.slurm
 done
