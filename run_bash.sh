c6b8b866b005f478b41bbb2ac8b1e38706b4910e
Di Huang <dh_599@hpc-login3.hpcc.usc.edu>
2019-04-12 13:45:48 -0700

    keep this version

--- run_bash.sh
+++ run_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for DOM in internet ; do
-    for METH in lap; do
+for DOM in social internet biology; do
+    for METH in rand; do
         sbatch --export=METH=$METH,DOM=$DOM exp_16core_benchmark.slurm
     done
 done
